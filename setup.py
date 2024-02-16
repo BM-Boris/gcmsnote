8a853c7bde37e7c5fa2a0a289ecbbd2dbb7517f8
BM-Boris <bb.minasenko@gmail.com>
2024-02-16 16:57:46 -0500

    restructured

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='gcmsnote',
     version='0.1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
     package_data={
         'gcmsnote': ['data/gcms_lib.csv'],
     },
